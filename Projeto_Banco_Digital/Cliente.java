422d501f52cdd484c53e6dfb48c4d5429d036bd9
Eduardo dos Santos Martins <128929388+ItsTheDuds@users.noreply.github.com>
2026-03-19 00:18:40 +0000

    feat: Adicionando os metodos lerTexto e lerValor em telas e organizando os metodos de Cliente

--- Projeto_Banco_Digital/Cliente.java
+++ Projeto_Banco_Digital/Cliente.java
@@ -29,17 +29,19 @@ public class Cliente {
     }
 
     // ----- CPF -----
-    public String getCpf() {
-        return cpf;
-    }
-
     public void setCpf(String cpf) {
+        int limiteMaximo = 11;
+        if (cpf == null) {
+        throw new IllegalArgumentException("CPF não pode ser nulo.");
+        }
+        if (cpf.length() > limiteMaximo) {
+        throw new IllegalArgumentException("O texto excede o limite de " + limiteMaximo + " caracteres.");
+        }
         this.cpf = cpf;
     }
 
-    // ----- IDADE -----
-    public int getIdade() {
-        return idade;
+    public String getCpf() {
+        return cpf;
     }
 
     // ----- DATA DE NASCIMENTO -----
@@ -63,6 +65,12 @@ public class Cliente {
         this.dataNascimento = dataNascimento;
         this.idade = idadeCalculada;
     }
+
+    // ----- IDADE -----
+    public int getIdade() {
+        return idade;
+    }
+
 
     // ----- EMAIL -----
     public String getEmail() {
--- Projeto_Banco_Digital/Telas.java
+++ Projeto_Banco_Digital/Telas.java
@@ -1,9 +1,26 @@
 package Projeto_Banco_Digital;
+import java.util.Scanner;
 
 public class Telas {
+    private static final Scanner sc = new Scanner(System.in);
 
+    // Leitura de dados do usuario
+    public static String lerTexto(String label) {
+        System.out.println(label + ": ");
+        return sc.nextLine().trim();
+    }
+
+    public static double lerValor(String label) {
+        System.out.println(label + ": ");
+        try {
+            return Double.parseDouble(sc.nextLine().trim().replace(".",","));
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
     // Menu inicial (antes do login)
     public void mostrarMenuCadastro() {
+        limparTela();
         System.out.println("===== BANCO DIGITAL =====");
         System.out.println("1 - Cadastrar Cliente");
         System.out.println("2 - Login");
@@ -13,6 +30,7 @@ public class Telas {
 
     // Menu depois de logar
     public void mostrarMenuPrincipal() {
+        limparTela();
         System.out.println("===== MENU PRINCIPAL =====");
         System.out.println("1 - Ver saldo");
         System.out.println("2 - Depositar");
@@ -29,11 +47,29 @@ public class Telas {
 
     // Mensagem de erro
     public void mostrarMensagemErro(String msg) {
+        limparTela();
         System.out.println("Erro: " + msg);
+        System.out.println("Pressione qualquer tecla para continuar...");
+        sc.nextLine();
     }
 
     // Mensagem genérica
     public void mostrarMensagem(String msg) {
         System.out.println(msg);
     }
+
+    public int lerOpcao() {
+        try {
+        int opcao = Integer.parseInt(sc.nextLine().trim());
+        return opcao;
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+
+    public static void limparTela() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
+   }
 }
